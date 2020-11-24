692b9434b045242821ee0ff92de15f6f88f46aa5
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-11-24 12:15:58 +0000

    attempt number 30000001

--- install-docker.sh
+++ install-docker.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
-curl https://get.docker.com | sudo bash
+curl https://get.docker.com |  bash
 curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
